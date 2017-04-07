3aa9d2026ae038a0670ab11e3b49e44ffecc8839
huwei <wei.hu@haoqitech.com.cn>
2017-04-07 18:51:12 +0800

    第一次提交

---
+++ app/Front.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: huwei
+ * Date: 2017/4/7
+ * Time: 18:23
+ */
+
+namespace App;
+
+
+class Front
+{
+    
+}
---
+++ app/Http/Controllers/Front/FrontLoginController.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Http\Controllers\Front;
+/**
+ * Created by PhpStorm.
+ * User: huwei
+ * Date: 2017/4/7
+ * Time: 18:25
+ */
+use App\Http\Controllers\Controller;
+class FrontLoginController extends Controller
+{
+    public function index()
+    {
+        return view('front.register');
+    }
+
+    public function register()
+    {
+
+    }
+
+    public function login()
+    {
+
+    }
+}
